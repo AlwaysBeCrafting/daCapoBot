4d1b403c1ae668eab6519df62edc8fd7136593af
Mark Halls <fireraisr@gmail.com>
2016-11-20 18:05:02 -0700

    output current playlist list

--- src/main/java/stream/alwaysbecrafting/daCapoBot/Main.java
+++ src/main/java/stream/alwaysbecrafting/daCapoBot/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main( String[] args ) {
 //		SoundJLayer soundToPlay = new SoundJLayer("test.mp3");
 //		soundToPlay.play();
-		new SoundJLayer( "test.mp3" ).play();
+//		new SoundJLayer( "test.mp3" ).play();
 		Playlist playlist = new Playlist();
 		playlist.Playlist( "/home/mh/Music/OC ReMix - 1 to 3000 [v20141015]" );
 	}
--- src/main/java/stream/alwaysbecrafting/daCapoBot/Playlist.java
+++ src/main/java/stream/alwaysbecrafting/daCapoBot/Playlist.java
@@ -7,15 +7,13 @@ import java.util.Arrays;
 //==============================================================================
 public class Playlist {
 	//--------------------------------------------------------------------------
-//todo new playlist
-//todo 	set directory location? read from config file?
-//todo 	read list of files from directory into array
 //todo 	public readable map? no just an array, output array list with position + 1,
 //todo 	call section with position - 1
-
+//todo  get position of playlist, output position + 1 and filename
+//todo  play playlist from position
 //todo 	eventually set it to use a sqllite db for upvote/downvote, skip count ...
-	private File              f;
-	private boolean           shuffle  = false;
+	private File f;
+	private boolean shuffle = false;
 	private ArrayList<String> playlist;
 
 	public void setShuffle( boolean shuffle ) {
@@ -24,15 +22,25 @@ public class Playlist {
 
 
 	public void Playlist( String path ) {
-		this.f = new File(path);
-		this.playlist = new ArrayList<String>( Arrays.asList(f.list()) );
-		System.out.println(playlist.toString());
+		this.f = new File( path );
+		this.playlist = new ArrayList<String>( Arrays.asList( f.list() ) );
+
+		for ( int i = 0; i < playlist.size(); i++ ) {
+			System.out.format( "%d %s\n", i + 1, playlist.get( i ) );
+		}
+//		for ( String index : playlist
+//				) {
+//			System.out.println( (playlist.indexOf( index ) + 1) + " " + index );
+//
+//		}
 	}
 
 	public void Play() {
-		if ( shuffle == false ){
+		if ( shuffle == false ) {
+
+		}
+		//--------------------------------------------------------------------------
 	}
-	//--------------------------------------------------------------------------
 }
 //------------------------------------------------------------------------------
 
