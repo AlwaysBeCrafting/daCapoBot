c2ca2e147b18ecc04086decf56128c52728fa26d
Mark Halls <fireraisr@gmail.com>
2016-12-06 01:38:35 -0700

    added output file for current track

--- src/main/java/stream/alwaysbecrafting/daCapoBot/Player.java
+++ src/main/java/stream/alwaysbecrafting/daCapoBot/Player.java
@@ -1,6 +1,8 @@
 package stream.alwaysbecrafting.daCapoBot;
 
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.advanced.AdvancedPlayer;
@@ -38,10 +40,17 @@ public class Player extends PlaybackListener{
 							try {
 
 								System.out.println(currentTrack.toString());
+
+								PrintWriter writer = new PrintWriter( "/home/mh/Current_Track", "UTF-8" );
+								writer.println( currentTrack.toString() );
+								writer.close();
+
 								this.player.play();
 								nextTrack();
 							} catch ( JavaLayerException ex ) {
 								ex.printStackTrace();
+							} catch ( IOException e){
+								e.printStackTrace();
 							}
 					}
 					,"AudioPlayerThread" );
