aeac867cacb87d9d210ace57fee82d7d45e3b6b7
Mark Halls <fireraisr@gmail.com>
2016-12-07 16:01:37 -0700

    removed unnecessary threading operation from the Player class

--- src/main/java/stream/alwaysbecrafting/daCapoBot/Player.java
+++ src/main/java/stream/alwaysbecrafting/daCapoBot/Player.java
@@ -1,24 +1,24 @@
 package stream.alwaysbecrafting.daCapoBot;
 
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 import javafx.embed.swing.JFXPanel;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 //==============================================================================
-public class Player{
+public class Player {
 	//--------------------------------------------------------------------------
 	//private AdvancedPlayer player;
 	private MediaPlayer player;
-	private Thread playerThread;
 	private boolean playerRunning = false;
 	private Track currentTrack;
 	private Playlist currentPlaylist;
 
 	//--------------------------------------------------------------------------
-	public void setPlaylist(Playlist currentPlaylist){
+	public void setPlaylist( Playlist currentPlaylist ) {
 		this.currentPlaylist = currentPlaylist;
 		currentTrack = currentPlaylist.getTrack( 0 );
 	}
@@ -27,45 +27,35 @@ public class Player{
 
 
 	public void play() {
-		try {
-			if( playerRunning ){
-				player.stop();
-			}
-			if( !playerRunning ){
-				JFXPanel jfxPanel = new JFXPanel();
-			}
-
-			player = new MediaPlayer( new Media( currentTrack.toURIString()));
-
-			playerThread = new Thread(
-					() -> {
-							try {
-
-								System.out.println(currentTrack.toString());
-
-								PrintWriter writer = new PrintWriter( "/home/mh/Current_Track", "UTF-8" );
-								writer.println( currentTrack.toString() );
-								writer.close();
-								this.player.play();
-								player.setOnEndOfMedia( new Runnable() {
-									@Override public void run() {
-										nextTrack();
-									}
-								} );
-							} catch ( IOException e){
-								e.printStackTrace();
-							}
-					}
-					,"AudioPlayerThread" );
-
-			playerThread.start();
-			playerRunning = true;
-
-
-		} catch ( Exception ex ) {
-			ex.printStackTrace();
+		if ( playerRunning ) {
+			player.stop();
 		}
-	}
+		if ( !playerRunning ) {
+			JFXPanel jfxPanel = new JFXPanel();
+		}
+		player = new MediaPlayer( new Media( currentTrack.toURIString() ) );
+		System.out.println( currentTrack.toString() );
+
+		try {
+			PrintWriter writer = new PrintWriter( "/home/mh/Current_Track", "UTF-8" );
+			writer.println( currentTrack.toString() );
+			writer.close();
+		}
+		catch (FileNotFoundException e){
+			e.printStackTrace();
+		}
+		catch (UnsupportedEncodingException f){
+			f.printStackTrace();
+		}
+
+		this.player.play();
+		player.setOnEndOfMedia( new Runnable() {
+			@Override public void run() {
+				nextTrack();
+			}
+		} );
+		playerRunning = true;
+}
 
 	//--------------------------------------------------------------------------
 
