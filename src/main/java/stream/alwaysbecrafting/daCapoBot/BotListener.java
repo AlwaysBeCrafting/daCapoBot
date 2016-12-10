cf8be5fc83fe89c457609f5f98be25d1234c3023
Mark Halls <fireraisr@gmail.com>
2016-12-10 03:11:27 -0700

    added suggestions/help/whoami keywords to botlistener

--- src/main/java/stream/alwaysbecrafting/daCapoBot/BotListener.java
+++ src/main/java/stream/alwaysbecrafting/daCapoBot/BotListener.java
@@ -16,12 +16,19 @@ public class BotListener extends ListenerAdapter{
 		if(event.getUser().getRealName() != null) {
 			db.logChat( event.getUser().getRealName(), event.getMessage() );
 		}
-		if (event.getMessage().startsWith( "!veto" ))
+		if (event.getMessage().toLowerCase().startsWith( "!help" ))
+			event.respondWith( "!help, !veto, !request, !whoami, !suggestions"  );
+		if (event.getMessage().toLowerCase().startsWith( "!veto" ))
 			p1.nextTrack();
 		if (event.getMessage().toLowerCase().startsWith( "!request" ))
-			p1.request(event.getMessage().toLowerCase().toLowerCase());
-		if (event.getMessage().startsWith( "!test" ))
+			p1.request(event.getMessage().toLowerCase());
+		if (event.getMessage().toLowerCase().startsWith( "!whoami" ))
+			event.respondWith( "I'm your friendly music bot. For a list of commands type !help. " +
+					"for my source code visit https://github.com/AlwaysBeCrafting/daCapoBot" );
+		if (event.getMessage().toLowerCase().startsWith( "!test" ))
 			event.respondWith( "Slippy's not such a screw up after all" );
+		if (event.getMessage().toLowerCase().startsWith( "!suggestions" ))
+			event.respondWith( "Feature requests are welcome, please submit to https://github.com/AlwaysBeCrafting/daCapoBot/issues" );
 
 	}
 
