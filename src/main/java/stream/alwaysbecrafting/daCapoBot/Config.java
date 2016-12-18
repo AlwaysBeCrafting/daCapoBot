d0e882e70c7e4cbe6f214d440a73636a09d329ea
Mark Halls <fireraisr@gmail.com>
2016-12-18 05:48:29 -0700

    successfully connected to twitch chat

--- src/main/java/stream/alwaysbecrafting/daCapoBot/Config.java
+++ src/main/java/stream/alwaysbecrafting/daCapoBot/Config.java
@@ -1,6 +1,7 @@
 package stream.alwaysbecrafting.daCapoBot;
 
 import org.pircbotx.Configuration;
+import org.pircbotx.cap.EnableCapHandler;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -18,8 +19,8 @@ class Config {
 	private static final List<String> propertiesList = Arrays.asList(
 			"music_directory"
 			,"bot_name"
-			,"irc_server"
-			,"irc_channel");
+			,"irc_channel"
+			,"oauth");
 
 	Config(){
 		properties();
@@ -29,11 +30,19 @@ class Config {
 	Configuration getBotConfig() {
 		//Configure what we want our bot to do
 		Configuration botConfiguration = new Configuration.Builder()
-				.setName( props.getProperty( "bot_name" ) ) //Set the nick of the bot.
+
+				.setAutoNickChange(false) //Twitch doesn't support multiple users
+				.setOnJoinWhoEnabled(false) //Twitch doesn't support WHO command
+				.setCapEnabled(true)
+				.addCapHandler(new EnableCapHandler("twitch.tv/membership")) //Twitch by default doesn't send JOIN, PART, and NAMES unless you request it, see https://github.com/justintv/Twitch-API/blob/master/IRC.md#membership
+
 				.addServer( props.getProperty( "irc_server" )) //Join the network
+				.setName( props.getProperty( "bot_name" ) ) //Set the nick of the bot.
+				.setServerPassword(props.getProperty( "oauth" )) //Your oauth password from http://twitchapps.com/tmi
 				.addAutoJoinChannel( props.getProperty( "irc_channel" ) )
 				.addListener( new BotListener() ) //Add our listener that will be called on Events
 				.buildConfiguration();
+
 		return botConfiguration;
 
 	}
@@ -49,8 +58,9 @@ class Config {
 			try ( OutputStream output = new FileOutputStream( "dacapobot.properties" ) ) {
 				props.setProperty( "music_directory", "$HOME/Music" );
 				props.setProperty( "bot_name", "YOUR_BOT_NAME_HERE" );
-				props.setProperty( "irc_server", "127.0.0.1" );
+				props.setProperty( "irc_server", "irc.twitch.tv" );
 				props.setProperty( "irc_channel", "#test" );
+				props.setProperty( "oauth", "BIGOATHTOKENHERE" );
 				props.store(output, null);
 			} catch ( IOException io ) {
 				io.printStackTrace();
