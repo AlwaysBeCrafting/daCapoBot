3203056305c839c3969b5c9d29003a1392e78727
Mark Halls <fireraisr@gmail.com>
2016-12-21 18:26:59 -0700

    responses to request and veto are more verbose refactored request and veto db methods to get string output instead of boolean veto method in player now tries to match current track first

--- src/main/java/stream/alwaysbecrafting/dacapobot/BotListener.java
+++ src/main/java/stream/alwaysbecrafting/dacapobot/BotListener.java
@@ -1,6 +1,5 @@
 package stream.alwaysbecrafting.dacapobot;
 
-import org.pircbotx.User;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.ConnectEvent;
 import org.pircbotx.hooks.events.MessageEvent;
@@ -9,70 +8,82 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 //==============================================================================
-class BotListener extends ListenerAdapter{
+class BotListener extends ListenerAdapter {
 	//--------------------------------------------------------------------------
 	private final Player PLAYER = new Player();
+	private static final Pattern pattern = Pattern.compile( "^(\\S+)\\s*(.+)?" );
 
 	@Override
-	public void onMessage(MessageEvent event) throws Exception{
-		if(event.getUser().getRealName() != null) {
-			Database.DB_INSTANCE.logChat( event.getUser().getRealName(), event.getMessage() );
+	public void onMessage( MessageEvent event ) throws Exception {
+		if ( event.getUser().getNick() != null ) {
+			Database.DB_INSTANCE.logChat( event.getUser().getNick(), event.getMessage() );
 		}
 
-		User getUser = event.getUser();
+		String nick = event.getUser().getNick();
 		String eventMessage = event.getMessage().toLowerCase();
 
-		Pattern requestRegex = Pattern.compile("!request\\s*(\\S+)");
-		Matcher requestMatcher = requestRegex.matcher( eventMessage );
+		Matcher matcher = pattern.matcher( eventMessage );
 
-		Pattern vetoRegex = Pattern.compile("!veto\\s*(\\S+)");
-		Matcher vetoMatcher = vetoRegex.matcher( eventMessage );
+		while ( matcher.find() ) {
 
-		if (event.getMessage().toLowerCase().startsWith( "!help" ))
-			event.respondWith( "!whoru, !help, !veto, !request, !suggestions"  );
+			switch ( matcher.group( 1 ) ) {
 
-		while ( vetoMatcher.find() ) {
-			System.out.println( "veto group 0: " + vetoMatcher.group( 1 ) );
-			if ( getUser != null ) {
-				if ( !PLAYER.veto( getUser.toString()
-						, vetoMatcher.group( 1 ) ) ) {
+				case "!veto":
+					if ( nick != null && matcher.group( 2 ) != null ) {
+						String veto = PLAYER.veto( nick, matcher.group( 2 ) );
+						if ( veto.startsWith( "Private:" ) ) {
+							event.respondWith( veto.replaceAll( "Private:\\s*", "" ) );
+						}
+						if ( veto.startsWith( "Public:" )){
+							event.respondWith( veto.replaceAll( "Public:\\s+", "" ) );
+						}
+					}
+					break;
 
-					event.respondWith( "Sorry, I can't find "
-							+ vetoMatcher.group(1)
-							+ ". Could you be specific?" );
-				}
+				case "!request":
+					if ( nick != null && matcher.group( 2 ) != null ) {
+						String request = PLAYER.request( nick, matcher.group( 2 ) );
+						if ( request.startsWith( "Private:" )){
+							event.respondWith( request.replaceAll( "Private:\\s*", "" ) );
+						}
+						if ( request.startsWith( "Public:" )){
+							event.respondWith( request.replaceAll( "Public:\\s*", "" ) );
+						}
+					}
+					break;
+
+				case "!whoru":
+					event.respondWith( "I'm your friendly music bot. For a list of commands type !help. " +
+							"For my source code visit https://github.com/AlwaysBeCrafting/dacapobot" );
+					break;
+
+				case "!test":
+					event.respondWith( "Slippy's not such a screw up after all" );
+					break;
+
+				case "!suggestions":
+					event.respondWith( "Feature requests are welcome, please submit to https://github.com/AlwaysBeCrafting/dacapobot/issues" );
+					break;
+
+				case "!help":
+					event.respondWith( "!whoru, !help, !veto, !request, !suggestions" );
+					break;
+
+				default:
+					break;
 			}
 		}
-
-		while ( requestMatcher.find() ) {
-			System.out.println("request group: " + requestMatcher.group(1));
-			if ( getUser != null ) {
-				event.respondWith( PLAYER.request( getUser.toString()
-						, requestMatcher.group( 1 ) ) );
-
-			}
-		}
-		if (event.getMessage().toLowerCase().startsWith( "!whoru" ))
-			event.respondWith( "I'm your friendly music bot. For a list of commands type !help. " +
-					"for my source code visit https://github.com/AlwaysBeCrafting/dacapobot" );
-
-		if (event.getMessage().toLowerCase().startsWith( "!test" ))
-			event.respondWith( "Slippy's not such a screw up after all" );
-
-		if (event.getMessage().toLowerCase().startsWith( "!suggestions" ))
-			event.respondWith( "Feature requests are welcome, please submit to https://github.com/AlwaysBeCrafting/dacapobot/issues" );
 
 	}
 
 	@Override
-	public void onConnect( ConnectEvent event ){
+	public void onConnect( ConnectEvent event ) {
 		try {
-			Database.DB_INSTANCE.addMP3s(new Config().getMusicDir());
+			Database.DB_INSTANCE.addMP3s( new Config().getMusicDir() );
 
 			this.PLAYER.setQueue();
 			this.PLAYER.play();
-		}
-		catch(Exception e){e.printStackTrace();}
+		} catch ( Exception e ) {e.printStackTrace();}
 	}
 
 	//--------------------------------------------------------------------------
