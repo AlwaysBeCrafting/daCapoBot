7cc8ba47a3f46f80fdcc632c28cec50d71507495
Mark Halls <fireraisr@gmail.com>
2017-01-10 17:51:31 -0700

    fixed race condition in track title file output

--- src/main/java/stream/alwaysbecrafting/dacapobot/Player.java
+++ src/main/java/stream/alwaysbecrafting/dacapobot/Player.java
@@ -10,52 +10,49 @@ import javafx.scene.media.MediaPlayer;
 
 import static stream.alwaysbecrafting.dacapobot.Database.DB_INSTANCE;
 
-//==============================================================================
 class Player {
-	//--------------------------------------------------------------------------
 	private MediaPlayer player;
 	private long timestamp = System.currentTimeMillis();
 	private Track currentTrack;
+	private Media media;
 
-	Player() {}
+	Player() {
+		JFXPanel jfxPanel = new JFXPanel();
+	}
 
 	void setQueue() {
-		JFXPanel jfxPanel = new JFXPanel();
-
 		Track nextTrack = null;
 		while(nextTrack == null || !nextTrack.exists()) {
 			nextTrack = DB_INSTANCE.getRandomTrack();
 			this.currentTrack = nextTrack;
 		}
-
-		System.out.println( "queue " + this.currentTrack );
+		storeTrackTitle( currentTrack.title );
 	}
 
 	void play() {
+		currentTrack.fetchTrackData();
+		media = new Media( currentTrack.toURIString() );
+		player = new MediaPlayer( media );
+		player.setOnEndOfMedia( () -> nextTrack() );
+		player.play();
+		System.out.println( "Now Playing: " + currentTrack.title );
+	}
 
-		stop();
-		player = new MediaPlayer( new Media( currentTrack.toURIString() ) );
-
-		if ( currentTrack.fetchTrackData() ) {
-			System.out.println( "Now Playing: " + currentTrack.title );
-		}
-
+	private void storeTrackTitle( String s ) {
 		try {
 			PrintWriter writer = new PrintWriter( Config.CONFIG.props.getProperty( "live_track_file" ), "UTF-8" );
-			writer.println( currentTrack.title );
+			writer.println( s );
 			writer.close();
 		} catch ( FileNotFoundException | UnsupportedEncodingException e ) {
 			e.printStackTrace();
 		}
-
-		this.player.play();
-		player.setOnEndOfMedia( this::nextTrack );
 	}
 
 	void nextTrack() {
+		stop();
+		storeTrackTitle( "" );
 		Track nextTrack = null;
 		while(nextTrack == null || !nextTrack.exists()) {
-
 			Track requestedTrack = Database.DB_INSTANCE.getNextRequested( timestamp );
 			if ( requestedTrack != null ) {
 				timestamp = requestedTrack.timestamp;
@@ -64,8 +61,16 @@ class Player {
 				timestamp = System.currentTimeMillis();
 				nextTrack = Database.DB_INSTANCE.getRandomTrack();
 			}
+			if( nextTrack == null || !nextTrack.exists() ) {
+				try {
+					Thread.sleep( 200 );
+				} catch ( InterruptedException e ) {
+					e.printStackTrace();
+				}
+			}
 		}
 		this.currentTrack = nextTrack;
+		storeTrackTitle( currentTrack.title );
 		play();
 	}
 
@@ -74,16 +79,8 @@ class Player {
 	}
 
 	public void stop() {
-		if ( player != null && player.getStatus() == MediaPlayer.Status.PLAYING ) {
+		if ( player != null ) {
 			player.stop();
-			try {
-				PrintWriter writer = new PrintWriter( Config.CONFIG.props.getProperty( "live_track_file" ), "UTF-8" );
-				writer.println( "" );
-				writer.close();
-			} catch ( FileNotFoundException | UnsupportedEncodingException e ) {
-				e.printStackTrace();
-			}
 		}
 	}
 }
-//------------------------------------------------------------------------------
