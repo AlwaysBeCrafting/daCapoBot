94f04e5fb6a0cf05a39c3476a90cae65b0f895f6
Arun Sahadeo <arunjamessahadeo@gmail.com>
2017-03-02 00:15:10 +0000

    Bot commands

--- src/main/java/stream/alwaysbecrafting/dacapobot/bot/BotListener.java
+++ src/main/java/stream/alwaysbecrafting/dacapobot/bot/BotListener.java
@@ -4,6 +4,8 @@ import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.DisconnectEvent;
 import org.pircbotx.hooks.events.MessageEvent;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -21,19 +23,19 @@ import stream.alwaysbecrafting.dacapobot.player.Player;
 
 public class BotListener extends ListenerAdapter {
 	private final Random RANDOM = new Random();
-	private static final Pattern pattern = Pattern.compile( "^(\\S+)(\\s+(.*))?" );
+	private static final Pattern pattern = Pattern.compile("^(\\S+)(\\s+(.*))?");
 	private Config config;
 	private Database database;
 	private Player player;
 
-	public BotListener( Config config, Database database, Player player ) {
+	public BotListener(Config config, Database database, Player player) {
 		this.config = config;
 		this.database = database;
 		this.player = player;
 	}
 
 	@Override
-	public void onDisconnect( DisconnectEvent event ) throws Exception{
+	public void onDisconnect(DisconnectEvent event) throws Exception {
 		player.exit();
 		database.close();
 		event.getBot().close();
@@ -48,6 +50,29 @@ public class BotListener extends ListenerAdapter {
 		String nick = event.getUser().getNick();
 		String eventMessage = event.getMessage().toLowerCase();
 		Matcher matcher = pattern.matcher( eventMessage );
+		String[] slot_number = {
+				"3:00PM",
+				"3:15PM",
+				"3:30PM",
+				"3:40PM",
+				"3:45PM",
+				"4:00PM",
+				"4:15PM",
+				"4:30PM",
+				"5:00PM",
+				"5:15PM",
+				"5:30PM",
+				"6:00PM"
+		};
+
+		List<String> zone_times = new ArrayList<String>();
+		zone_times.addAll(config.getTimeZones());
+		for (int i = 0; i < zone_times.size(); i++) {
+			if (zone_times.get(i).startsWith("SystemV") || zone_times.get(i).startsWith("GMT")) {
+				zone_times.remove(i);
+			}
+		}
+
 		if ( matcher.find() ) {
 			List<String> matcherArgs = Collections.emptyList();
 			if (matcher.group(3) != null) {
@@ -56,185 +81,205 @@ public class BotListener extends ListenerAdapter {
 						.collect( Collectors.toList() );
 			}
 			switch ( matcher.group( 1 ) ) {
-				case "!shutdown":
-					Optional isAdmin = config.getAdmins().stream().filter( s -> s.equals( nick ) ).findAny();
-					if ( isAdmin.isPresent() ) {
-						event.respondWith( "Clumsy Robot stopped moving!" );
+				case "!shutdown": {
+					Optional isAdmin = config.getAdmins().stream().filter(s -> s.equals(nick)).findAny();
+					if (isAdmin.isPresent()) {
+						event.respondWith("Clumsy Robot stopped moving!");
 						event.getBot().send().quitServer();
 					} else {
-						event.respondWith( "I'm sorry " + nick + ", I'm afraid I can't do that." );
+						event.respondWith("I'm sorry " + nick + ", I'm afraid I can't do that.");
 					}
 					break;
-
-				case "!time":
+				}
+				case "!time": {
 					String current_time = config.getTime();
 
-					if ( current_time != null ) {
-						event.respondWith( "The time is " + current_time );
+					if (current_time != null) {
+						event.respondWith("The time is " + current_time);
 					} else {
-						event.respondWith( "No" );
+						event.respondWith("No");
 					}
 					break;
-
-				case "!timezones":
-					List<String> zone_times = new ArrayList<String>();
-					zone_times.addAll(config.getTimeZones());
-					for ( int i = 0; i < zone_times.size(); i++ ) {
-						if ( zone_times.get(i).startsWith( "SystemV" ) || zone_times.get(i).startsWith( "GMT" ))
-						{
-							zone_times.remove(i);
-						}
-					}
-					String current_tz = config.props.getProperty( "timezone" );
-					boolean contains = zone_times.contains(config.props.getProperty( "timezone" ));
-
-					if ( !contains ) {
-						event.respondWith( "No" );
-						
-					} else {
-						event.respondWith( "Your timezone is " + current_tz);
-					}
-					break;
-
-				case "!veto":
-					if ( nick != null && matcher.group( 3 ) != null ) {
-						if ( matcherArgs.isEmpty() ){
-							event.respondWith( "Sorry, " + matcher.group(3) + " is too vague. See '!help !veto'" +
-									" for more info." );
-						}
-						else if (matcherArgs.get(0).toLowerCase().matches( "[remix]{1,5}" )){
-							event.respondWith( "Illegal first argument " + matcherArgs.get(0) + " too similar to 'remix'" );
-						}
-						else if ( player.getCurrentTitle()
+				}
+				case "!veto": {
+					if (nick != null && matcher.group(3) != null) {
+						if (matcherArgs.isEmpty()) {
+							event.respondWith("Sorry, " + matcher.group(3) + " is too vague. See '!help !veto'" +
+									" for more info.");
+						} else if (matcherArgs.get(0).toLowerCase().matches("[remix]{1,5}")) {
+							event.respondWith("Illegal first argument " + matcherArgs.get(0) + " too similar to 'remix'");
+						} else if (player.getCurrentTitle()
 								.toLowerCase()
-								.replaceAll( "[^a-z0-9]+", "%" )
-								.contains( ( matcher.group(3).replaceAll( "[^a-z0-9]+", "%" ) ) ) ) {
-							database.addVeto( nick, database.searchTracksByTitle( player.getCurrentTitle() ).get( 0 ));
-							event.respondWith( player.getCurrentTitle() + " vetoed." );
+								.replaceAll("[^a-z0-9]+", "%")
+								.contains((matcher.group(3).replaceAll("[^a-z0-9]+", "%")))) {
+							database.addVeto(nick, database.searchTracksByTitle(player.getCurrentTitle()).get(0));
+							event.respondWith(player.getCurrentTitle() + " vetoed.");
 							player.nextTrack();
 						} else {
-							List<TrackMetadata> trackList = database.searchTracksByTitle( matcherArgs.toString() );
+							List<TrackMetadata> trackList = database.searchTracksByTitle(matcherArgs.toString());
 
-							if ( trackList.isEmpty() ) {
-								event.respondWith( "Sorry, I couldn't find any tracks containing " + matcher.group( 2 ) );
+							if (trackList.isEmpty()) {
+								event.respondWith("Sorry, I couldn't find any tracks containing " + matcher.group(2));
 							}
-							if ( trackList.size() == 1 ) {
-								database.addVeto( nick, trackList.get( 0 ) );
-								event.respondWith( trackList.get( 0 ).title + "vetoed." );
+							if (trackList.size() == 1) {
+								database.addVeto(nick, trackList.get(0));
+								event.respondWith(trackList.get(0).title + "vetoed.");
 							}
-							if ( trackList.size() > 1 ) {
+							if (trackList.size() > 1) {
 								String response = "";
-								for ( int i = 0; i < Math.min( trackList.size(), 3 ); i++ ) {
-									if ( "".equals( response ) ) {
-										response = response + trackList.get( i ).title;
+								for (int i = 0; i < Math.min(trackList.size(), 3); i++) {
+									if ("".equals(response)) {
+										response = response + trackList.get(i).title;
 									} else {
-										response = response + " ❙ " + trackList.get( i ).title;
+										response = response + " ❙ " + trackList.get(i).title;
 									}
 								}
-								if ( trackList.size() > 3 ) {
-									response = response + " ❙ +" + ( trackList.size() - 3 ) + " more";
+								if (trackList.size() > 3) {
+									response = response + " ❙ +" + (trackList.size() - 3) + " more";
 								}
-								event.respondWith( response );
+								event.respondWith(response);
 							}
 						}
 					}
 					break;
+				}
 
-				case "!request":
-					if ( matcherArgs.isEmpty() ) {
-						event.respondWith( "Sorry, " + matcher.group( 3 ) + " is too vague. See '!help !request'" +
-								" for more info." );
+				case "!request": {
+					if (matcherArgs.isEmpty()) {
+						event.respondWith("Sorry, " + matcher.group(3) + " is too vague. See '!help !request'" +
+								" for more info.");
+						break;
+					} else if (matcherArgs.get(0).toLowerCase().matches("[remix]{1,5}")) {
+						event.respondWith("Illegal first argument " + matcherArgs.get(0) + " too similar to 'remix'");
 						break;
 					}
-					else if (matcherArgs.get(0).toLowerCase().matches( "[remix]{1,5}" )){
-						event.respondWith( "Illegal first argument " + matcherArgs.get(0) + " too similar to 'remix'" );
-						break;
-					}
-					else if (matcherArgs.get(0).toLowerCase().matches( "--random" ) && matcherArgs.get(1).isEmpty()){
-						event.respondWith( "--random requires a partial title." );
-						break;
-					}
-					else if (matcherArgs.get(0).toLowerCase().matches( "--random" ) && !matcherArgs.get(1).isEmpty()){
+					else if (matcherArgs.get(0).toLowerCase().matches("--slot=1 --random") && !matcherArgs.get(1).isEmpty()) {
+
+						String current_tz = config.props.getProperty("timezone");
+						boolean contains = zone_times.contains(config.props.getProperty("timezone"));
+
+						if (contains) {
+							event.respondWith("The timezone is valid, proceeding...");
+							}
+							else {
+							event.respondWith("You have specified an invalid timezone.");
+							return;
+						}
+
 						String randomRequest = "";
-						for ( int i = 1; i < matcherArgs.size(); i++ ) {
-							if("".equals( randomRequest )){
+						for (int i = 1; i < matcherArgs.size(); i++) {
+							if ("".equals(randomRequest)) {
 								randomRequest = matcherArgs.get(i);
-							}else {
-								randomRequest = randomRequest + " " + matcherArgs.get( i );
+							} else {
+								randomRequest = randomRequest + " " + matcherArgs.get(i);
+							}
+
+							List<TrackMetadata> matchingTracks = database.searchTracksByTitle(randomRequest);
+							if (matchingTracks.isEmpty()) {
+								event.respondWith("Sorry, I couldn't find any tracks containing " + matcher.group(3).replaceAll("--random ", ""));
+							} else if (matchingTracks.size() == 1) {
+								TrackMetadata lastInRequest = database.getFinalFromRequests();
+
+								if (lastInRequest != null && matchingTracks.get(0).title.equalsIgnoreCase(lastInRequest.title)) {
+									event.respondWith(matchingTracks.get(0).title + " is the last song in the request list. Please choose a different track.");
+								}
+								if (matchingTracks.size() == 1 && !matchingTracks.get(0).title.equalsIgnoreCase(lastInRequest.title)) {
+									database.addRequest(nick, matchingTracks.get(0));
+									event.respondWith(matchingTracks.get(0).title + " added to the queue.");
+								}
+							} else if (matchingTracks.size() > 1) {
+								int randomTrack = RANDOM.nextInt(matchingTracks.size());
+								database.addRequest(nick, matchingTracks.get(randomTrack));
+								event.respondWith(matchingTracks.get(randomTrack).title + " added to the queue.");
+							}
+						}
+						break;
+					} else if (matcherArgs.get(0).toLowerCase().matches("--random") && matcherArgs.get(1).isEmpty()) {
+						event.respondWith("--random requires a partial title.");
+						break;
+					} else if (matcherArgs.get(0).toLowerCase().matches("--random") && !matcherArgs.get(1).isEmpty()) {
+						String randomRequest = "";
+						for (int i = 1; i < matcherArgs.size(); i++) {
+							if ("".equals(randomRequest)) {
+								randomRequest = matcherArgs.get(i);
+							} else {
+								randomRequest = randomRequest + " " + matcherArgs.get(i);
 							}
 						}
 
-						List<TrackMetadata> matchingTracks = database.searchTracksByTitle( randomRequest );
-						if ( matchingTracks.isEmpty() ) {
-							event.respondWith( "Sorry, I couldn't find any tracks containing " + matcher.group(3).replaceAll( "--random ", "" ));
-						}
-						else if ( matchingTracks.size() == 1 ){
+						List<TrackMetadata> matchingTracks = database.searchTracksByTitle(randomRequest);
+						if (matchingTracks.isEmpty()) {
+							event.respondWith("Sorry, I couldn't find any tracks containing " + matcher.group(3).replaceAll("--random ", ""));
+						} else if (matchingTracks.size() == 1) {
 							TrackMetadata lastInRequest = database.getFinalFromRequests();
 
-							if ( lastInRequest != null && matchingTracks.get( 0 ).title.equalsIgnoreCase( lastInRequest.title ) ) {
-								event.respondWith( matchingTracks.get( 0 ).title + " is the last song in the request list. Please choose a different track.");
+							if (lastInRequest != null && matchingTracks.get(0).title.equalsIgnoreCase(lastInRequest.title)) {
+								event.respondWith(matchingTracks.get(0).title + " is the last song in the request list. Please choose a different track.");
 							}
-							if ( matchingTracks.size() == 1 && !matchingTracks.get( 0 ).title.equalsIgnoreCase( lastInRequest.title )){
-								database.addRequest( nick, matchingTracks.get( 0 ) );
-								event.respondWith( matchingTracks.get( 0 ).title + " added to the queue." );
+							if (matchingTracks.size() == 1 && !matchingTracks.get(0).title.equalsIgnoreCase(lastInRequest.title)) {
+								database.addRequest(nick, matchingTracks.get(0));
+								event.respondWith(matchingTracks.get(0).title + " added to the queue.");
 							}
-						}
-						else if ( matchingTracks.size() > 1 ) {
+						} else if (matchingTracks.size() > 1) {
 							int randomTrack = RANDOM.nextInt(matchingTracks.size());
-							database.addRequest( nick, matchingTracks.get(randomTrack) );
-							event.respondWith( matchingTracks.get(randomTrack).title + " added to the queue.");
+							database.addRequest(nick, matchingTracks.get(randomTrack));
+							event.respondWith(matchingTracks.get(randomTrack).title + " added to the queue.");
 						}
-					}
-					else if ( nick != null && matcher.group( 3 ) != null ) {
+					} else if (nick != null && matcher.group(3) != null) {
 						TrackMetadata lastInRequest = database.getFinalFromRequests();
-						List<TrackMetadata> matchingTracks = database.searchTracksByTitle( matcher.group( 3 ).toString() );
+						List<TrackMetadata> matchingTracks = database.searchTracksByTitle(matcher.group(3).toString());
 
-						if ( matchingTracks.isEmpty() ) {
-							event.respondWith( "Sorry, I couldn't find any tracks containing " + matcher.group(3));
+						if (matchingTracks.isEmpty()) {
+							event.respondWith("Sorry, I couldn't find any tracks containing " + matcher.group(3));
 						}
-						if ( matchingTracks.size() > 1 ) {
+						if (matchingTracks.size() > 1) {
 							String response = "";
-							for ( int i = 0; i < Math.min( matchingTracks.size(), 3 ); i++ ) {
-								if ( "".equals( response ) ) {
-									response = response + matchingTracks.get( i ).title;
+							for (int i = 0; i < Math.min(matchingTracks.size(), 3); i++) {
+								if ("".equals(response)) {
+									response = response + matchingTracks.get(i).title;
 								} else {
-									response = response + " ❙ " + matchingTracks.get( i ).title;
+									response = response + " ❙ " + matchingTracks.get(i).title;
 								}
 							}
-							if ( matchingTracks.size() > 3 ) {
-								response = response + " ❙ +" + ( matchingTracks.size() - 3 ) + " more";
+							if (matchingTracks.size() > 3) {
+								response = response + " ❙ +" + (matchingTracks.size() - 3) + " more";
 							}
-							event.respondWith( response );
+							event.respondWith(response);
 						}
-						if ( lastInRequest != null && matchingTracks.get( 0 ).title.equalsIgnoreCase( lastInRequest.title ) ) {
-							event.respondWith( matchingTracks.get( 0 ).title + " is the last song in the request list. Please choose a different track.");
+						if (lastInRequest != null && matchingTracks.get(0).title.equalsIgnoreCase(lastInRequest.title)) {
+							event.respondWith(matchingTracks.get(0).title + " is the last song in the request list. Please choose a different track.");
 						}
-						if ( matchingTracks.size() == 1 && !matchingTracks.get( 0 ).title.equalsIgnoreCase( lastInRequest.title )){
-							database.addRequest( nick, matchingTracks.get( 0 ) );
-							event.respondWith( matchingTracks.get( 0 ).title + " added to the queue." );
+						if (matchingTracks.size() == 1 && !matchingTracks.get(0).title.equalsIgnoreCase(lastInRequest.title)) {
+							database.addRequest(nick, matchingTracks.get(0));
+							event.respondWith(matchingTracks.get(0).title + " added to the queue.");
 						}
 					}
 					break;
+				}
 
-				case "!whoru":
-					event.respondWith( "I'm your friendly music bot. For a list of commands type !help. " +
-							"For my source code visit https://github.com/AlwaysBeCrafting/dacapobot" );
+				case "!whoru": {
+					event.respondWith("I'm your friendly music bot. For a list of commands type !help. " +
+							"For my source code visit https://github.com/AlwaysBeCrafting/dacapobot");
 					break;
+				}
 
-				case "!test":
-					event.respondWith( "Slippy's not such a screw up after all" );
+				case "!test": {
+					event.respondWith("Slippy's not such a screw up after all");
 					break;
+				}
 
-				case "!suggestions":
-					event.respondWith( "Feature requests are welcome, please submit to https://github.com/AlwaysBeCrafting/dacapobot/issues" );
+				case "!suggestions": {
+					event.respondWith("Feature requests are welcome, please submit to https://github.com/AlwaysBeCrafting/dacapobot/issues");
 					break;
+				}
 
-				case "!admins":
-						List admin_list = config.getAdmins();
-						if (admin_list.size() > 0) {
-							event.respondWith("The admins are: " + admin_list);
-							break;
-						}
+				case "!admins": {
+					List admin_list = config.getAdmins();
+					if (admin_list.size() > 0) {
+						event.respondWith("The admins are: " + admin_list);
+						break;
+					}
+				}
 
 				case "!help":
 					if (matcherArgs.isEmpty()) {
